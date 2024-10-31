5888e3b6c7376de599e162120c6c769b467fae9e
nirvanasupermind <nirvana.supermind@gmail.com>
2024-10-31 02:16:01 -0700

    function gives segfault

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,13 @@
     
 int main(){
 tachyon_stl_setup();
-(*tachyon_internal::decode_func(print))({tachyon_internal::get_subscript(tachyon_internal::make_str(new std::string("Hello world!")),0)});
+double Complex=tachyon_internal::make_obj(new TACHYON_OBJ({{"add",tachyon_internal::make_func(new TACHYON_FUNC([=](const std::vector<double>& _args) -> double {
+double self= _args.at(0);
+double other= _args.at(1);
+{
+return tachyon_internal::make_obj(new TACHYON_OBJ({{"proto",Complex},{"real",(tachyon_internal::get_prop(tachyon_internal::decode_obj(self),"real"))+(tachyon_internal::get_prop(tachyon_internal::decode_obj(other),"real"))},{"imag",(tachyon_internal::get_prop(tachyon_internal::decode_obj(self),"imag"))+(tachyon_internal::get_prop(tachyon_internal::decode_obj(other),"imag"))}}));
+}}))}}));
+tachyon_internal::get_prop(tachyon_internal::decode_obj(Complex),"add");
 
 tachyon_internal::free_all();
 return 0;
