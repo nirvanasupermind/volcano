063760a7e8783037b0c32095535afe5933d19fa0
nirvanasupermind <nirvana.supermind@gmail.com>
2024-11-16 21:31:07 -0800

    lexical grammar ebnf finished

--- test/test.cpp
+++ test/test.cpp
@@ -3,12 +3,9 @@
     
 int main(){
 tachyon_stl_setup();
-{
-}
-(*tachyon_internal::decode_func(println))({(1.00000000000000000)&&(0.0)});
-(*tachyon_internal::decode_func(println))({(1.00000000000000000)||(1.00000000000000000)});
-(*tachyon_internal::decode_func(println))({(9.00000000000000000)||(7.00000000000000000)});
-(*tachyon_internal::decode_func(println))({!(12.0000000000000000)});
+double x=1.00000000000000000;
+x&&=(0.0);
+(*tachyon_internal::decode_func(println))({x});
 
 tachyon_internal::free_all();
 return 0;
